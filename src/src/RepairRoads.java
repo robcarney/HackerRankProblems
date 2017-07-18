537ff9dbb1a761c260151010d7bf1f46731da2b2
carneyrob <robert.edward.carney@gmail.com>
2017-07-18 00:24:57 -0400

    Turning in

--- src/src/RepairRoads.java
+++ src/src/RepairRoads.java
@@ -1,6 +1,9 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -18,31 +21,78 @@ public class RepairRoads {
     }
   }
 
-  public static class Graph  {
-    List<Road> graph;
+  public static class Network  {
+    int num;
+    List<Road> roads;
 
-    public Graph() {
-      this.graph = new ArrayList<>();
+    public Network(int n) {
+      this.num = n;
+      this.roads = new ArrayList<>();
+    }
+
+    public void addRoad(Road r)  {
+      this.roads.add(r);
+    }
+  }
+
+  public static class Node  {
+    int id;
+    List<Integer> neighbors;
+
+    Node(int id, List<Integer> neighbors) {
+      this.id = id;
+      this.neighbors = neighbors;
+    }
+
+    void addNeightbor(int n)  {
+      if (neighbors.contains(n))  {
+        return;
+      }
+      neighbors.add(n);
     }
   }
 
 
   public static void main(String[] args)  {
     Scanner sc = new Scanner(System.in);
-    Graph[] cases = new Graph[Integer.parseInt(sc.nextLine())];
+    Network[] cases = new Network[Integer.parseInt(sc.nextLine())];
     int count = -1;
-    int curr = 0;
     while (sc.hasNextLine())  {
       String[] line = sc.nextLine().split(" ");
       if (line.length == 1)  {
-        curr = Integer.parseInt(line[0]);
-        Graph graph = new Graph();
+        int curr = Integer.parseInt(line[0]);
         count += 1;
+        cases[count] = new Network(curr);
       }
       else  {
-        
+        Road r = new Road(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
+        cases[count].addRoad(r);
       }
     }
+  }
+
+  private static int getNumRobots(Network n)  {
+    List<Node> adjacencyList = getAdjacencyList(n);
+    HashMap<Integer, Boolean> visited = new HashMap<>();
+    Queue<Integer> toVisit = new LinkedList<>();
+    
+  }
+
+  private static List<Node> getAdjacencyList(Network n)  {
+    HashMap<Integer,Node> map = new HashMap<>();
+    for (Road r : n.roads)  {
+      int i = r.start;
+      int j = r.end;
+      if (map.get(i) == null)  {
+        map.put(i,new Node(i,new ArrayList<>()));
+      }
+      if (map.get(j) == null)  {
+        map.put(j,new Node(j,new ArrayList<>()));
+      }
+      map.get(i).addNeightbor(j);
+      map.get(j).addNeightbor(i);
+    }
+    return new ArrayList<>(map.values());
   }
 
 }
