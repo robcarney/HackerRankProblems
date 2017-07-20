a0e2d6129be9afdec43d640b5e1dcc6524f71603
carneyrob <robert.edward.carney@gmail.com>
2017-07-20 19:02:07 -0400

    Still not fast enough

--- src/RoadsAndLibraries.java
+++ src/RoadsAndLibraries.java
@@ -1,6 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,11 +10,6 @@ public class RoadsAndLibraries {
   public static class Graph  {
     HashMap<Integer, List<Integer>> adj;
     int v;
-
-    public Graph(HashMap<Integer, List<Integer>> adj) {
-      this.v = 0;
-      this.adj = adj;
-    }
 
     public Graph(int v)  {
       this.v = v;
@@ -32,7 +25,7 @@ public class RoadsAndLibraries {
     }
 
     public int numDisconnected()  {
-      int result = 1;
+      int result = 0;
       List<Integer> iter = new ArrayList<>(this.adj.keySet());
       for (int k : iter) {
         if (adj.get(k).size() == 0)  {
@@ -40,49 +33,23 @@ public class RoadsAndLibraries {
           result++;
         }
       }
-      List<Integer> connected = this.depthFirstSearch();
-      for (int j : connected)  {
-        this.adj.remove(j);
-      }
-      while (!this.adj.keySet().isEmpty())  {
-        result++;
-        connected = this.depthFirstSearch();
-        for (int j : connected)  {
-          this.adj.remove(j);
-        }
-      }
-      return result;
-    }
-
-    List<Integer> depthFirstSearch()  {
-      ArrayList<Integer> result = new ArrayList<>();
-      if (adj.keySet().size() == 0)  {
-        return result;
-      }
-      if (adj.keySet().size() == 1)  {
-        for (int i : adj.keySet())  {
-          result.add(i);
-        }
-        return result;
-      }
-      int start = -1;
-      for (int i : adj.keySet())  {
-        if (adj.get(i).isEmpty())  { continue; }
-        else  {
-          start = i;
-          break;
-        }
-      }
-      if (start == -1)  {
-        for (int i : adj.keySet())  {
-          result.add(i);
-          return result;
-        }
-      }
       HashMap<Integer,Boolean> explored = new HashMap<>();
       for (int i : adj.keySet())  {
         explored.put(i,false);
       }
+      while (!this.adj.keySet().isEmpty())  {
+        result++;
+        this.depthFirstSearch(explored);
+      }
+      return result;
+    }
+
+    void depthFirstSearch(HashMap<Integer,Boolean> explored)  {
+      if (adj.keySet().size() == 0)  {
+        this.adj = new HashMap<>();
+        return;
+      }
+      int start = adj.keySet().iterator().next();
       Stack<Integer> stack = new Stack<>();
       stack.push(start);
       while (!stack.empty())  {
@@ -92,12 +59,10 @@ public class RoadsAndLibraries {
           for (int i : adj.get(curr))  {
             stack.push(i);
           }
-          result.add(curr);
+          this.adj.remove(curr);
         }
       }
-      return result;
     }
-
   }
 
   public static long cost(long cLib, long cRoad, Graph g, int gSize)  {
@@ -110,6 +75,7 @@ public class RoadsAndLibraries {
 
 
   public static void main(String[] args) {
+    long startTime = System.nanoTime();
     File file = new File("input1.txt");
     List<Long> costs = new ArrayList<>();
     try {
@@ -121,12 +87,12 @@ public class RoadsAndLibraries {
         int m = in.nextInt();
         long x = in.nextLong();
         long y = in.nextLong();
-        for(int a1 = 0; a1 < m; a1++){
+        for (int a1 = 0; a1 < m; a1++) {
           int city_1 = in.nextInt() - 1;
           int city_2 = in.nextInt() - 1;
-          g.addEdge(city_1,city_2);
+          g.addEdge(city_1, city_2);
         }
-        costs.add(cost(x,y,g,n));
+        costs.add(cost(x, y, g, n));
       }
     } catch (Exception ex)  {
       ex.printStackTrace();
@@ -135,9 +101,7 @@ public class RoadsAndLibraries {
     for (long l : costs)  {
       System.out.println(l);
     }
-    /*
-    Graph g = new Graph(9);
-    g.addEdge(7,1);
-    g.addEdge(1,8);*/
+    long elapsedTime = System.nanoTime() - startTime;
+    System.out.println((double) elapsedTime / 1000000000.0);
   }
 }
