37fb9ddcde088504ed2ae837937bc893106f2d50
Robert Carney <robert.edward.carney@gmail.com>
2017-08-05 16:47:50 -0400

    More stuff

--- src/Abbreviation.java
+++ src/Abbreviation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by robertcarney on 8/4/17.
@@ -13,11 +14,12 @@ public class Abbreviation {
         for (int i = 0; i < str.length(); i++)  {
             String curr = str.substring(i,i+1);
             boolean uppercase = !curr.toLowerCase().equals(curr);
-            for (int j = targets.size() - 1; j >= 0; j++)  {
+            for (int j = targets.size() - 1; j >= 0; j--)  {
                 String s = targets.get(j);
                 String letter = s.substring(0,1);
                 if (letter.equalsIgnoreCase(curr))  {
                     if (s.length() == 1)  {
+                        System.out.println("YES");
                         return true;
                     }
                     targets.add(s.substring(1,s.length()));
@@ -27,12 +29,18 @@ public class Abbreviation {
                 }
             }
         }
+        System.out.println("NO");
         return false;
     }
 
 
     public static void main(String[] args)  {
-        System.out.println(isAbb("daBcd", "ABC"));
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for (int i = 0; i<q; i++)  {
+            isAbb(sc.next(), sc.next());
+        }
+        System.out.println(isAbb("daBcd", "ACB"));
     }
 
 }
