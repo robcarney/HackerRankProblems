952cc3c3bce4672767e88ec5d375120a7bbc0136
Robert Carney <robert.edward.carney@gmail.com>
2017-08-05 16:48:49 -0400

    Rollback

--- src/Abbreviation.java
+++ src/Abbreviation.java
@@ -35,11 +35,6 @@ public class Abbreviation {
 
 
     public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
-        int q = sc.nextInt();
-        for (int i = 0; i<q; i++)  {
-            isAbb(sc.next(), sc.next());
-        }
         System.out.println(isAbb("daBcd", "ACB"));
     }
 
