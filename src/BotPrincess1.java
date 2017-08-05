9e85fb41781086ba8eab5ea1f8104abb4f73fe0c
Robert Carney <robert.edward.carney@gmail.com>
2017-08-05 18:47:53 -0400

    Too easy

--- src/BotPrincess1.java
+++ src/BotPrincess1.java
@@ -1,5 +1,53 @@
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by robertcarney on 8/5/17.
  */
 public class BotPrincess1 {
+
+    public static void main(String[] args)  {
+        File f = new File("input.txt");
+        try  {
+            Scanner s = new Scanner(f);
+            int n = Integer.parseInt(s.nextLine());
+            int botX = 0;
+            int botY = 0;
+            int princessX = 0;
+            int princessY = 0;
+            for (int i = 0; i < n; i++)  {
+                String curr = s.nextLine();
+                if (curr.contains("m"))  {
+                    botX = curr.indexOf("m");
+                    botY = i;
+                }
+                if (curr.contains("p"))  {
+                    princessX = curr.indexOf("p");
+                    princessY = i;
+                }
+            }
+            while (botX != princessX || botY != princessY)  {
+                int xDiff = botX - princessX;
+                int yDiff = botY - princessY;
+                if (xDiff > 0)  {
+                    System.out.println("LEFT");
+                    botX--;
+                }
+                if (xDiff < 0)  {
+                    System.out.println("RIGHT");
+                    botX++;
+                }
+                if (yDiff > 0)  {
+                    System.out.println("UP");
+                    botY--;
+                }
+                if (yDiff < 0)  {
+                    System.out.println("DOWN");
+                    botY++;
+                }
+            }
+        } catch (Exception ex)  {
+            ex.printStackTrace();
+        }
+    }
 }
