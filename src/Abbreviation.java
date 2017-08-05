38d83104fc7079002e74d41331405d0eb0626345
Robert Carney <robert.edward.carney@gmail.com>
2017-08-05 16:44:39 -0400

    Starting on abbreviation

--- src/Abbreviation.java
+++ src/Abbreviation.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by robertcarney on 8/4/17.
  */
 public class Abbreviation {
+
+
+    static boolean isAbb(String str, String target)  {
+        List<String> targets = new ArrayList<>();
+        targets.add(target);
+        for (int i = 0; i < str.length(); i++)  {
+            String curr = str.substring(i,i+1);
+            boolean uppercase = !curr.toLowerCase().equals(curr);
+            for (int j = targets.size() - 1; j >= 0; j++)  {
+                String s = targets.get(j);
+                String letter = s.substring(0,1);
+                if (letter.equalsIgnoreCase(curr))  {
+                    if (s.length() == 1)  {
+                        return true;
+                    }
+                    targets.add(s.substring(1,s.length()));
+                }
+                else if (uppercase) {
+                    targets.remove(j);
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args)  {
+        System.out.println(isAbb("daBcd", "ABC"));
+    }
+
 }
