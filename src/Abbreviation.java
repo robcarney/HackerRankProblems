0f4d135c6b4bcc75b911e4cb3a20a8b89a0ac7ad
Robert Carney <robert.edward.carney@gmail.com>
2017-08-05 18:10:28 -0400

    Almost there

--- src/Abbreviation.java
+++ src/Abbreviation.java
@@ -10,6 +10,41 @@ public class Abbreviation {
 
 
     static boolean isAbb(String str, String target)  {
+        boolean[][] memo = new boolean[str.length()+1][target.length()+1];
+        memo[0][0] = true;
+        int strSize = str.length();
+        int tSize = target.length();
+        for (int i = strSize; i > 0; i--)  {
+            String curr = str.substring(i-1,strSize);
+            if (curr.equals(curr.toLowerCase()))  {
+                memo[strSize - i + 1][0] = true;
+            }
+        }
+        for (int i = strSize; i > 0; i--)  {
+            int iIndex = strSize - i + 1;
+            String currS = str.substring(i-1,i);
+            boolean uppercase = !currS.equals(currS);
+            for (int j = tSize; j > 0; j--)  {
+                int jIndex = tSize - j + 1;
+                String currT = target.substring(j-1,j);
+                if (currS.equalsIgnoreCase(currT))  {
+                    memo[iIndex][jIndex] = (memo[iIndex-1][jIndex-1] || memo[iIndex-1][jIndex]);
+                }
+                else if (uppercase)  {
+                    memo[iIndex][jIndex] = false;
+                }
+                else  {
+                    memo[iIndex][jIndex] = memo[iIndex-1][jIndex];
+                }
+            }
+        }
+        if (memo[strSize][tSize])  {
+            System.out.println("YES");
+            return true;
+        }
+        System.out.println("NO");
+        return false;
+            /*
         List<String> targets = new ArrayList<>();
         targets.add(target);
         for (int i = 0; i < str.length(); i++)  {
@@ -40,9 +75,19 @@ public class Abbreviation {
             }
         }
         System.out.println("NO");
-        return false;
+        return false;*/
     }
 
+
+    static void printBoolArray(boolean[][] arr)  {
+        for (boolean[] x : arr)  {
+            for (boolean b : x)  {
+                String curr = (b) ? "1 " : "0 ";
+                System.out.print(curr);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args)  {
         File f = new File("input.txt");
