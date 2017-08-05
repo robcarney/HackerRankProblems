7fe2034555bcd95702aca52b1f256fd24bb0c388
Robert Carney <robert.edward.carney@gmail.com>
2017-08-05 18:19:23 -0400

    Clean up

--- src/Abbreviation.java
+++ src/Abbreviation.java
@@ -49,38 +49,6 @@ public class Abbreviation {
         }
         System.out.println("NO");
         return false;
-            /*
-        List<String> targets = new ArrayList<>();
-        targets.add(target);
-        for (int i = 0; i < str.length(); i++)  {
-            if (targets.isEmpty())  {
-                System.out.println("NO");
-                return false;
-            }
-            String curr = str.substring(i,i+1);
-            boolean uppercase = !curr.toLowerCase().equals(curr);
-            for (int j = targets.size() - 1; j >= 0; j--)  {
-                String s = targets.get(j);
-                String letter = "";
-                if (s.length() > 0)  {
-                    letter = s.substring(0,1);
-                }
-                if (letter.equalsIgnoreCase(curr))  {
-                    targets.add(s.substring(1,s.length()));
-                }
-                else if (uppercase) {
-                    targets.remove(j);
-                }
-            }
-        }
-        for (String s : targets)  {
-            if (s.length() == 0)  {
-                System.out.println("YES");
-                return true;
-            }
-        }
-        System.out.println("NO");
-        return false;*/
     }
 
 
