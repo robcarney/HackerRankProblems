e74fd455a51e6473ade64fe34f0a12e04ecb586e
Robert Carney <robert.edward.carney@gmail.com>
2017-08-06 11:48:56 -0400

    More stuff

--- src/EqualCandies.java
+++ src/EqualCandies.java
@@ -2,4 +2,16 @@
  * Created by robertcarney on 8/6/17.
  */
 public class EqualCandies {
+
+    static int numOperations(int[] nums)  {
+        int min = 10000;
+        for (int i = 0; i < nums.length; i++)  {
+            
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
