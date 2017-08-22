5d02725702b901b163d821c225223f5e0a03985f
Robert Carney <robert.edward.carney@gmail.com>
2017-08-22 14:19:41 -0400

    Progress

--- src/BricksGame.java
+++ src/BricksGame.java
@@ -9,11 +9,28 @@ public class BricksGame {
 
     static long playBrickGame(int[] bricks)  {
         long result = 0;
-        long[] memo = new long[bricks.length];
-        long[] sums = new long[bricks.length];
-        int[] revArray = new int[bricks.length];
-        for (int i = 0; i < bricks.length; i++)  {
-
+        int blength = bricks.length;
+        long[] memo = new long[blength;
+        long[] sums = new long[blength];
+        int[] revArray = new int[blength];
+        for (int i = 0; i < blength; i++)  {
+            revArray[blength-i-1] = bricks[i];
+        }
+        long curr = 0;
+        for (int i = 0; i < blength; i++)  {
+            curr += revArray[i];
+            sums[i] = curr;
+        }
+        if (blength < 4)  {
+            return sums[blength-1];
+        }
+        memo[0] = sums[0];
+        memo[1] = sums[1];
+        memo[2] = sums[2];
+        for (int i = 3; i < blength; i++)  {
+            long one = memo[i-3];
+            long two = memo[i-2];
+            
         }
     }
 
