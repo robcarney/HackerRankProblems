98dd725041560c83abf277f3cba35db950c9c54d
Robert Carney <robert.edward.carney@gmail.com>
2017-08-26 12:50:58 -0400

    Progress

--- src/LIS.java
+++ src/LIS.java
@@ -1,5 +1,18 @@
+import java.io.File;
+
 /**
  * Created by robertcarney on 8/26/17.
  */
 public class LIS {
+
+
+
+    public static void main(String[] args)  {
+        File f = new File("input.txt");
+        try  {
+
+        } catch (Exception ex) {
+            
+        }
+    }
 }
