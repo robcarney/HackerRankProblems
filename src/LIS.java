c884fc7c00753e24eb9f39f068dc0b380fe1065b
Robert Carney <robert.edward.carney@gmail.com>
2017-08-26 12:54:05 -0400

    Progress

--- src/LIS.java
+++ src/LIS.java
@@ -1,18 +1,29 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by robertcarney on 8/26/17.
  */
 public class LIS {
 
+    static int longestIS(int[] seq)  {
+        ArrayList<ArrayList<Integer>> memo = new ArrayList<>();
+        
+    }
 
 
     public static void main(String[] args)  {
         File f = new File("input.txt");
         try  {
-
+            Scanner sc = new Scanner(f);
+            int n = sc.nextInt();
+            int[] seq = new int[n];
+            for (int i = 0; i < n; i++)  {
+                seq[i] = sc.nextInt();
+            }
         } catch (Exception ex) {
-            
+
         }
     }
 }
