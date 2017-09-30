fa5a419aaff50fb8e0ea54babce689aa8a69e60e
robcarney <robert.edward.carney@gmail.com>
2017-09-30 19:25:39 -0400

    First one maybe done

--- src/MthToLastElement.java
+++ src/MthToLastElement.java
@@ -1,7 +1,30 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 /**
  * Created by robertcarney on 9/30/17.
  */
 public class MthToLastElement {
+
+
+    public static void main(String[] args) {
+        File f = new File("input.txt");
+        try  {
+            Scanner sc = new Scanner(f);
+            LinkedList<Integer> l = new LinkedList<>();
+            int n = sc.nextInt();
+            while (sc.hasNextInt())  {
+                l.add(sc.nextInt());
+            }
+            int count = 1;
+            while (count < n)  {
+                l.pollLast();
+                count++;
+            }
+            System.out.println(l.getLast());
+        } catch (Exception ex)  {
+            ex.printStackTrace();
+        }
+    }
 }
