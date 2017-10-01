72f10104eda51257af8c0a7083b586631cdf8812
robcarney <robert.edward.carney@gmail.com>
2017-09-30 20:22:22 -0400

    New problem

---
+++ src/FibLite.java
@@ -0,0 +1,9 @@
+/**
+ * Created by robertcarney on 9/30/17.
+ */
+public class FibLite {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/MthToLastElement.java
+++ src/MthToLastElement.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -11,24 +13,26 @@ public class MthToLastElement {
     public static void main(String[] args) {
         File f = new File("input.txt");
         try  {
-            Scanner sc = new Scanner(f);
+            BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
             LinkedList<Integer> l = new LinkedList<>();
-            int n = sc.nextInt();
-            int i = 0;
-            while (sc.hasNextInt())  {
-                l.add(sc.nextInt());
-                i++;
-            }
+            int n = Integer.parseInt(sc.readLine());
+            String[] strings = sc.readLine().split(" ");
+            int i = strings.length;
             if (i < n)  {
                 System.out.println("NIL");
                 return;
+            }
+            System.out.println(strings[i-n]);
+            /*
+            for (String str : strings)  {
+                l.add(Integer.parseInt(str));
             }
             int count = 1;
             while (count < n)  {
                 l.pollLast();
                 count++;
             }
-            System.out.println(l.getLast());
+            System.out.println(l.getLast());*/
         } catch (Exception ex)  {
             ex.printStackTrace();
         }
