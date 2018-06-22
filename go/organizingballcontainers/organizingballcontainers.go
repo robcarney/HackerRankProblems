5f41d0318f3f16f97f7ac6c93266e05b8707d192
Robert Carney <rcarney@basistech.com>
2018-06-22 14:33:28 -0400

    Adding new problem skeleton

---
+++ go/organizingballcontainers/organizingballcontainers.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the organizingContainers function below.
+func organizingContainers(container [][]int32) string {
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	qTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	for qItr := 0; qItr < int(q); qItr++ {
+		nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		var container [][]int32
+		for i := 0; i < int(n); i++ {
+			containerRowTemp := strings.Split(readLine(reader), " ")
+
+			var containerRow []int32
+			for _, containerRowItem := range containerRowTemp {
+				containerItemTemp, err := strconv.ParseInt(containerRowItem, 10, 64)
+				checkError(err)
+				containerItem := int32(containerItemTemp)
+				containerRow = append(containerRow, containerItem)
+			}
+
+			if len(containerRow) != int(int(n)) {
+				panic("Bad input")
+			}
+
+			container = append(container, containerRow)
+		}
+
+		result := organizingContainers(container)
+
+		fmt.Fprintf(writer, "%s\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
