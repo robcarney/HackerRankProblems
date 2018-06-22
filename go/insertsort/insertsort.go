8f9aa6af47711da09c342db8cf3c027a04befed2
Robert Carney <rcarney@basistech.com>
2018-06-22 15:05:02 -0400

    Adding insertion sort problem

---
+++ go/insertsort/insertsort.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the insertionSort function below.
+func insertionSort(arr []int32) int32 {
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		arrTemp := strings.Split(readLine(reader), " ")
+
+		var arr []int32
+
+		for i := 0; i < int(n); i++ {
+			arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+			checkError(err)
+			arrItem := int32(arrItemTemp)
+			arr = append(arr, arrItem)
+		}
+
+		result := insertionSort(arr)
+
+		fmt.Fprintf(writer, "%d\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
