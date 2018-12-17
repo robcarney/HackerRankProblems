5128eb2a83084da8122572b6848b22bc0405cd16
Rob Carney <robert.edward.carney@gmail.com>
2018-12-16 19:46:22 -0500

    Basic solution for even tree

--- python/even_tree.py
+++ python/even_tree.py
@@ -1,33 +1,58 @@
 #!/bin/python3
 
 import math
-import sys
 import os
 import random
 import re
 import sys
+import time
 
 USE_STDIN = False
 
 # Complete the evenForest function below.
 def evenForest(graph):
-    return nodesInTree(graph, 0)
+    memo = [0] * len(graph)
+    getSubtreeAncestors(graph, 0, memo)
+    return evenForestSubtree(graph, memo, 0)
+
+def evenForestSubtree(graph, memo, start, parent=-1):
+    edgesRemoved = 0
+    toRemove = []
+    for node in graph[start]:
+        if node == parent:
+            continue
+        if memo[node] % 2 == 0:
+            toRemove.append(node)
+            edgesRemoved += 1
+            memo[start] -= memo[node]
+            edgesRemoved += evenForestSubtree(graph, memo, node, start)
+    for node in toRemove:
+        removeEdge(graph, start, node)
+    for node in graph[start]:
+        if node == parent:
+            continue
+        edgesRemoved += evenForestSubtree(graph, memo, node, start)
+    return edgesRemoved
+
+        
+def removeEdge(graph, parent, child):
+    graph[parent].remove(child)
+    graph[child].remove(parent)
 
 
-def nodesInTree(graph, start):
-    toExplore = [start]
-    explored = {}
-    for i in range(len(graph)):
-        explored[i] = False
-    count = 0
-    while toExplore:
-        curr = toExplore.pop()
-        explored[curr] = True
-        count += 1
-        for node in graph[curr]:
-            if not explored[node]:
-                toExplore.append(node)
-    return count
+
+def getSubtreeAncestors(graph, start, memo, parent=-1):
+    for node in graph[start]:
+        if len(graph[node]) == 1:
+            memo[node] = 1
+        elif node == parent:
+            continue
+        else:
+            getSubtreeAncestors(graph, node, memo, start)
+    count = 1
+    for node in graph[start]:
+        count += memo[node]
+    memo[start] = count
 
 
 def buildGraph(t_nodes, t_edges, t_from, t_to):
@@ -54,10 +79,7 @@ if __name__ == '__main__':
 
     for i in range(t_edges):
         t_from[i], t_to[i] = map(int, inf.readline().rstrip().split())
-    print(t_to)
-    print(t_from)
     graph = buildGraph(t_nodes, t_edges, t_to, t_from)
-    print(graph)
 
     res = evenForest(graph)
 
