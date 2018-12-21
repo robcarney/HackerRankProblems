adddb98201b0ff2e4ce6c55201776caa5344321b
Rob Carney <robert.edward.carney@gmail.com>
2018-12-21 00:17:36 -0500

    New years chaos mostly solved

--- python/new_year_chaos.py
+++ python/new_year_chaos.py
@@ -10,35 +10,56 @@ USE_STDIN = False
 
 # Complete the minimumBribes function below.
 def minimumBribes(q):
+    if (not isValid(q)):
+        print("Too chaotic")
+        return
+    bribes = countInversions(q)
+    print(bribes)
+
+
+
+def isValid(q):
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            return False
+    return True
+
+
+def countInversions(q):
+    inversions, q = countInversionsHelper(q)
+    return inversions
+
+
+def countInversionsHelper(q):
     if len(q) <= 1:
         return 0, q
     else:
-        inv1, l1 = minimumBribes(q[:len(q)//2])
-        inv2, l2 = minimumBribes(q[len(q)//2:])
-        inv3 = merge(l1, l2)
+        split = len(q) // 2
+        inv1, l1 = countInversionsHelper(q[:split])
+        inv2, l2 = countInversionsHelper(q[split:])
+        inv3, q = merge(l1, l2)
+        return inv1 + inv2 + inv3, q
 
 
 def merge(l1, l2):
     c1 = 0 
     c2 = 0
     inv = 0
-    while (c1 < len(l1)):
-        if l1[c1] <= l1[c2]:
+    result = []
+    while (c1 < len(l1) and c2 < len(l2)):
+        if l1[c1] <= l2[c2]:
+            result.append(l1[c1])
             c1 += 1
-            c2 += 1
         else:
-            c1 += 1
-            inv += 1
-    return inv
-    
-    
-        
+            result.append(l2[c2])
+            c2 += 1
+            inv += len(l1) - c1
+    if (c1 < len(l1)):
+        result.extend(l1[c1:])
+    if (c2 < len(l2)):
+        result.extend(l2[c2:])
+    return inv, result
 
-def normalList(size):
-    result = [0] * size
-    for i in range(size):
-        result[i] = i
-    return result
 
 
 if __name__ == '__main__':
@@ -53,8 +74,4 @@ if __name__ == '__main__':
 
         q = list(map(int, inf.readline().rstrip().split()))
 
-        inv, l = minimumBribes(q)
-        if (inv > len(q)):
-            print("Too chaotic")
-        else:
-            print(inv)
+        minimumBribes(q)
