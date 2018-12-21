207474da78032ff9f9308e20b5bb5c1e5bf245bc
Rob Carney <robert.edward.carney@gmail.com>
2018-12-21 01:47:33 -0500

    First solution for min swaps (too slow maybe)

--- python/min_swaps.py
+++ python/min_swaps.py
@@ -8,42 +8,24 @@ import sys
 
 USE_STDIN = False
 
-def countInversions(q):
-    inversions, q = countInversionsHelper(q)
-    return inversions
+def minSwaps(arr):
+    seen = []
+    swaps = 0
+    next = 1
+    for curr in arr:
+        if curr in seen:
+            continue
+        seen.append(curr)
+        next = arr[curr - 1]
+        count = 1
+        while next != curr:
+            count += 1
+            seen.append(next)
+            next = arr[next - 1]
+        swaps += count - 1
+    return swaps
 
 
-def countInversionsHelper(q):
-    if len(q) <= 1:
-        return 0, q
-    else:
-        split = len(q) // 2
-        inv1, l1 = countInversionsHelper(q[:split])
-        inv2, l2 = countInversionsHelper(q[split:])
-        print("Merging ", l1, l2)
-        inv3, q = merge(l1, l2)
-        print("Inversions: %d" % (inv1+inv2+inv3))
-        return inv1 + inv2 + inv3, q
-
-
-def merge(l1, l2):
-    c1 = 0 
-    c2 = 0
-    inv = 0
-    result = []
-    while (c1 < len(l1) and c2 < len(l2)):
-        if l1[c1] <= l2[c2]:
-            result.append(l1[c1])
-            c1 += 1
-        else:
-            result.append(l2[c2])
-            c2 += 1
-            inv += len(l1) - c1
-    if (c1 < len(l1)):
-        result.extend(l1[c1:])
-    if (c2 < len(l2)):
-        result.extend(l2[c2:])
-    return inv, result
 
 if __name__ == '__main__':
     if USE_STDIN:
@@ -55,6 +37,6 @@ if __name__ == '__main__':
 
     arr = list(map(int, inf.readline().rstrip().split()))
 
-    res = countInversions(arr)
+    res = minSwaps(arr)
 
     print(res)
