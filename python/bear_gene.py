0c5c41a805634f91743f4700456ea21aa3b8486c
Rob Carney <robert.edward.carney@gmail.com>
2018-12-28 16:06:27 -0500

    First pass at steady gene

--- python/bear_gene.py
+++ python/bear_gene.py
@@ -6,18 +6,102 @@ import random
 import re
 import sys
 
+from collections import namedtuple
+
+USE_STDIN = False
+
 # Complete the steadyGene function below.
-def steadyGene(gene):
+def steadyGeneOld(gene, geneLength):
+    geneDict = buildStringDict(gene)
+    evenCount = geneLength / 4
+    numberOff = 0
+    for c in geneDict.keys():
+        if geneDict[c] > evenCount:
+            numberOff += geneDict[c] - evenCount
+    MemoEntry = namedtuple("MemoEntry", "length dict")
+    memo = []
+    for i in range(n):
+        memo.append(MemoEntry(0, {}))
+    lastStart = -1
+    bestDict = {}
+    for i in range(0,n):
+        if geneDict[gene[i]] > evenCount:
+            if lastStart == -1:
+                lastStart = i
+                currNumOff = 1
+                j = i + 1
+                newDict = buildStringDict("")
+                while (currNumOff < numberOff):
+                    if geneDict[gene[j]] - newDict[gene[j]] > evenCount:
+                        currNumOff += 1
+                        newDict[gene[j]] += 1
+                    j += 1
+                memo[i] = MemoEntry(j - i, newDict)
+            else:
+                lastEntry = memo[lastStart]
+                if gene[lastEntry] == gene[i] and lastEntry.dict[gene[i]] > geneDict[gene[i]] - evenCount:
+                    newDict = lastEntry.dict
+                    newDict[gene[i]] -= 1
+    return numberOff
+
+
+def steadyGene(gene, geneLength):
+    rightbound, currDict = getRightBound(gene, geneLength)
+    if rightbound == 0:
+        return 0
+    evenCount = geneLength / 4
+    geneDict = buildStringDict(gene)
+    leftbound = 0
+    memo = [0] * geneLength
+    while rightbound < geneLength:
+        curr = gene[leftbound]
+        currDict[curr] += 1
+        while currDict[curr] > evenCount:
+            currDict[gene[rightbound]] -= 1
+            rightbound += 1
+        memo[leftbound] = rightbound - leftbound
+        leftbound += 1
+    best = 500000
+    for n in memo:
+        if n < best and n != 0:
+            best = n
+    return best
+
+
+
+
+def getRightBound(gene, geneLength):
+    currDict = buildStringDict("")
+    evenCount = geneLength / 4
+    bound = geneLength
+    for i in range(geneLength - 1, -1, -1):
+        if currDict[gene[i]] >= evenCount:
+            return bound, currDict
+        else:
+            currDict[gene[i]] += 1
+            bound -= 1
+    return bound, currDict
+
+
+
+def buildStringDict(gene):
+    result = {}
+    result['G'] = 0
+    result['C'] = 0
+    result['A'] = 0
+    result['T'] = 0
+    for c in gene:
+        result[c] += 1
+    return result
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    if USE_STDIN:
+        inf = sys.stdin
+    else:
+        inf = open('in.txt', 'r')
 
-    n = int(input())
+    n = int(inf.readline().replace("\n",""))
 
-    gene = input()
+    gene = inf.readline().replace("\n","")
 
-    result = steadyGene(gene)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(steadyGene(gene, n))
